32a27c5ffefe737da5f0f8a6aaad9f04b88ab5b8
shahadat10033 <shahadat10033@gmail.com>
2023-02-18 07:21:27 -0800

    rectangle section completed

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 function getInputValue(id){
-    return   document.getElementById(id).value;
+    return   document.getElementById(id).value ;
    }
 
    function getInputId(id){
@@ -14,20 +14,43 @@ document.getElementById("btn-triangle-calculate").addEventListener("click",funct
     const inputTriangleB=parseFloat(getInputValue("input-triangle-b" ));
     
     const inputTriangleH=parseFloat(getInputValue("input-triangle-h" ));
-    const triangleArea= 0.5* inputTriangleB*inputTriangleH;
+    const triangleArea=( 0.5* inputTriangleB*inputTriangleH) .toFixed(2);
     
-    const areaResultField=getInputId("area-result-field");
-    if(typeof triangleArea<=0 || inputTriangleB !==Number||inputTriangleH!==Number){
+    const AreaResultField=getInputId("area-result-field");
+    if( triangleArea<0   ){
         alert("plz type a valid number");
     }
     else{ let li=document.createElement("li");
     li.classList.add("list-group-item")
     li.innerHTML=`  Triangle     A = ${triangleArea }  cm<sup>2</sup> <button class="btn btn-primary">convert to m<sup>2</sup></button>`
  
-     areaResultField.appendChild( li )}
+    AreaResultField.appendChild( li )}
   
 
 })
 
 // -------------------------------------Triangle part end------------------------------------------------------
 
+// -------------------------------------Rectangle part starts------------------------------------------------------
+
+document.getElementById("btn-rectangle-calculate").addEventListener("click",function(){
+
+  
+    const inputRectangleW=parseFloat(getInputValue("input-rectangle-w" ));
+    
+    const inputRectangleL =parseFloat(getInputValue("input-rectangle-l" ));
+    const RectangleArea= (inputRectangleW*inputRectangleL).toFixed(2);
+    
+   const  areaResultField=getInputId("area-result-field");
+    if(RectangleArea<0 ){
+        alert("plz type a valid number");
+    }
+    else{ let li=document.createElement("li");
+    li.classList.add("list-group-item")
+    li.innerHTML=`  Rectangle     A = ${RectangleArea }  cm<sup>2</sup> <button class="btn btn-primary">convert to m<sup>2</sup></button>`
+ 
+     areaResultField.appendChild( li )}
+  
+
+})
+// -------------------------------------Rectangle part Ends------------------------------------------------------
